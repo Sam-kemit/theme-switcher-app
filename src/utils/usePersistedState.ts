3700d43e6758ed4a0118ceb4a610c41aa50db4a0
Samuel KUETA <samuel.kueta@gmail.com>
2020-08-30 15:46:01 +0200

    first commit

---
+++ src/utils/usePersistedState.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+type ResponseDTO<T> = [
+    T,
+    Dispatch<SetStateAction<T>>
+];
+
+function usePersistedState<T>(key: string, initialState: T): ResponseDTO<T> {
+    const [state, setState] = useState(() => {
+        const storageValue = localStorage.getItem(key);
+
+        if (storageValue) {
+            return JSON.parse(storageValue);
+        } else {
+            return initialState;
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+
+    return [state, setState];
+}
+
+export default usePersistedState;
